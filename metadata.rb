e50e9f3e887f3235f3c804983cb77679be7ea5ca
Justin Weiss <justin@uberweiss.org>
2010-07-27 08:43:47 -0700

    Add the appropriate apt keys for the 10gen repository

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Avvo, Inc."
 maintainer_email  "jweiss@avvo.com"
 license           "Apache 2.0"
 description       "Installs mongodb"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,15 +6,20 @@ execute "apt-update" do
   action :nothing
 end
 
+execute "add apt keys" do
+  command "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+  user 'root'
+  action :nothing
+  notifies :run, resources(:execute => "apt-update"), :immediately
+end
+
 # need alternate source for mongo
 template "/etc/apt/sources.list.d/mongodb.list" do
   source "mongodb.list.erb"
   owner "root"
   group "root"
   mode 0755
-  notifies :run, resources(:execute => "apt-update"), :immediately
+  notifies :run, resources(:execute => "add apt keys"), :immediately
 end
 
-package "mongodb-stable" do
-  options "--force-yes"
-end
+package "mongodb-stable"
